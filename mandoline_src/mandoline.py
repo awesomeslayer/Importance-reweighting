Edited generations, fixed problems, started to add LSCV-KL method for bandwidth scoring

mandoline_src/mandoline.py
@@ -1,14 +1,12 @@
 from functools import partial
 from types import SimpleNamespace
-from typing import Optional, List
+from typing import List, Optional
 
 import numpy as np
-
-from scipy.special import logsumexp
-
 import scipy.optimize
 import scipy.special
 import sklearn.metrics.pairwise as skmetrics
+from scipy.special import logsumexp
 
 
 def Phi(

source/estimations.py
@@ -1,26 +1,9 @@
 import numpy as np
 from scipy.special import logsumexp
-
-
-# def importance_sampling_error(err, p, g, g_sample):
-#     """
-#     :param err: log-error function
-#     :param p: log-probability density of target distribution
-#     :param g: log-probability density of sample distribution
-#     :param g_sample:
-#     :return: log-ISE
-#     """
-#     # print(f"error IS:{logsumexp(p(g_sample) - g(g_sample) + err(g_sample))}")
-#     return logsumexp(err(g_sample) + p(g_sample) - g(g_sample)) - np.log(
-#         g_sample.shape[0]
-#     )
-
+import logging
 
 def importance_sampling_error(err, p, g, g_sample):
-    sum = 0
-    for i in range(g_sample.shape[0]):
-        sum = sum + err([g_sample[i]]) * p([g_sample[i]]) / g([g_sample[i]])
-    return np.squeeze(sum) / g_sample.shape[0]
+    return logsumexp(err(g_sample) + p(g_sample) - g(g_sample)) - np.log(g_sample.shape[0])
 
 
 def monte_carlo_error(err, p_sample):
@@ -29,10 +12,7 @@ def monte_carlo_error(err, p_sample):
     :param p_sample:
     :return: log-MCE
     """
-    sum = 0
-    for i in range(p_sample.shape[0]):
-        sum = sum + err([p_sample[i]])
-    return np.squeeze(sum)/p_sample.shape[0]
+    return logsumexp(err(p_sample)) - np.log(p_sample.shape[0])
 
 
 def clip(a, b_min, b_max):
@@ -58,16 +38,16 @@ def ISE_clip(err, p, g, g_sample, eps, smooth_flag=True):
     :return: log-ISE with clip
     """
     clipped_array = []
-    if smooth_flag:
-        for p_elem, g_elem in zip(p(g_sample), g(g_sample)):
-            clipped_array.append(np.log(smooth_clip(p_elem/g_elem, eps)))
-    else:
-        for p_elem, g_elem in zip(p(g_sample), g(g_sample)):
-            clipped_array.append(
-                np.log(clip(p_elem/g_elem, 1 - eps, 1 + eps))
-            )
-    sum = 0
+    for p_elem, g_elem in zip(p(g_sample), g(g_sample)):
+        
+        
+        weight = np.exp(p_elem - g_elem)
+        if(np.isnan(weight)):
+            logging.info(weight)
+        if smooth_flag:
+            clipped_array.append(np.log(smooth_clip(weight, eps)))
+        else:
+            clipped_array.append(np.log(clip(weight, 1 - eps, 1 + eps)))
     
-    for i in range(g_sample.shape[0]):
-        sum = sum + clipped_array[i]*err([g_sample[i]])
-    return np.squeeze(sum)/g_sample.shape[0]
+    return logsumexp(clipped_array + err(g_sample)) - np.log(g_sample.shape[0])
+

source/mandoline_estimation.py
@@ -1,34 +1,35 @@
-import numpy as np
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+import numpy as np
 from scipy.special import logsumexp
-from sklearn.model_selection import KFold, ShuffleSplit
-from tqdm import trange
-from mandoline_src.mandoline import mandoline, log_density_ratio
+from sklearn.cluster import KMeans
+import logging
+
+from mandoline_src.mandoline import log_density_ratio, mandoline
 
 
-def mandoline_error(gen_dict, n_slices=3):
-    # for prediction-based slices:
-    # D_src = slice_prediction(g_test, model, n_slices=n_slices)
-    # D_tgt = slice_prediction(
-    #    p_test, model, n_slices=n_slices
-    # )
-    kmeans = KMeans(n_clusters=n_slices, n_init="auto")
-    kmeans.fit(gen_dict["g_test"])
+def mandoline_error(gen_dict, n_slices=3, slice_method = 'clusters'):
+    if(slice_method == 'pred'):
+        D_src = slice_prediction(gen_dict["g_test"], gen_dict["model"], n_slices=n_slices)
+        D_tgt = slice_prediction(gen_dict["p_test"], gen_dict["model"], n_slices=n_slices)
+    
+    elif(slice_method == 'clusters'):
+        kmeans = KMeans(n_clusters=n_slices, n_init="auto")
+        kmeans.fit(gen_dict["g_test"])
 
-    D_src = slice_clusterisation(gen_dict["g_test"], kmeans, n_slices=n_slices)
-    D_tgt = slice_clusterisation(gen_dict["p_test"], kmeans, n_slices=n_slices)
-
+        D_src = slice_clusterisation(gen_dict["g_test"], kmeans, n_slices=n_slices)
+        D_tgt = slice_clusterisation(gen_dict["p_test"], kmeans, n_slices=n_slices)
+    else:
+        logging.info(f"Bad slicing_method, choose correct!")
+        return False
     # Run the solver
     solved = mandoline(D_src, D_tgt, edge_list=None, sigma=1)
 
     # Compute the weights on the source dataset
-    density_ratios = np.e ** log_density_ratio(solved.Phi_D_src, solved)
+    log_density_ratios = log_density_ratio(solved.Phi_D_src, solved)
 
     # print smth for max_cov ?
-    return np.exp(logsumexp(np.log(density_ratios) + np.log(gen_dict["err"](gen_dict["g_test"]))) - np.log(gen_dict["g_test"].shape[0]))
-
+    return logsumexp(log_density_ratios + gen_dict["err"](gen_dict["g_test"]))- np.log(gen_dict["g_test"].shape[0])
+    
 
 def get_correct(model, X, labels, tolerance=1e-5):
     """

source/run.py
@@ -1,24 +1,16 @@
 from functools import partial
-import numpy as np
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import KFold, ShuffleSplit
-import matplotlib.pyplot as plt
-from tqdm import trange, tqdm
-from sklearn.neighbors import KernelDensity
-import logging
-from copy import copy
 
-from tests.metrics import mape, rmse
-from tests.test import test
-from .simulation import (
-    random_linear_func,
-    random_gaussian_mixture_func,
-    random_GMM_samples,
-    random_uniform_samples,
-    random_GP_func,
-    DummyModel,
-)
+from tqdm import trange
 
+from .simulation import (DummyModel, random_gaussian_mixture_func,
+                         random_GMM_samples, random_GP_func,
+                         random_linear_func, random_uniform_samples)
+
+from plots.extr_plots import extr_plots
+from .errors import errors_test, count_metrics 
+from .errors_init import errors_init, find_sizes
 
 def run(
     conf,
@@ -31,6 +23,7 @@ def run(
         "ISE_g_clip": [0],
         "ISE_g_estim_clip": [0],
     },
+    log = None
 ):
     f_gens = {
         "linear": partial(random_linear_func, conf),
@@ -51,237 +44,35 @@ def run(
         else ShuffleSplit(n_splits=1, test_size=0.3, random_state=0)
     )
 
-    #get sizes for hyperparams_search
-    sizes = find_sizes(params, hyperparams_dict)    
-    logging.info(f"\n sizes = {sizes}\n")
-    #init errors dictionaries
-    err_dict, err_hyp_dict = errors_init(params, sizes, list())
+    # get sizes for hyperparams_search
+    sizes = find_sizes(params, hyperparams_dict)
+    log.debug(f"\n sizes = {sizes}\n")
+    # init errors dictionaries
+    err_dict, err_hyp_dict = errors_init(params, sizes, list(), hyperparams_params["grid_flag"])
 
-    logging.info(f"\nerr_dict init = {err_dict}\n")
-    logging.info(f"\nerr_hyp_dict init = {err_hyp_dict}\n")
+    log.debug(f"\nerr_dict init = {err_dict}\n")
+    log.debug(f"\nerr_hyp_dict init = {err_hyp_dict}\n")
 
-    #test all methods, get errors dictionaries (for each method and each param on same generations (same folds) for each tests)
+    # test all methods, get errors dictionaries (for each method and each param on same generations (same folds) for each tests)
     for i in trange(params["n_tests"]):
-        errors_test(err_dict, err_hyp_dict, conf, params, hyperparams_dict, hyperparams_params, sizes, kf, i)
-    
-    #if needed find best hyperparams and get best_mape/rmse dictionaries with them
-    if hyperparams_params["grid_flag"]:
-        best_metrics_dict, metrics_dict = find_best_params(
-            params, hyperparams_dict, sizes, err_hyp_dict, err_dict
+        errors_test(
+            err_dict,
+            err_hyp_dict,
+            conf,
+            params,
+            hyperparams_dict,
+            hyperparams_params,
+            sizes,
+            kf,
+            i,
+            log
         )
-        logging.info(f"\nmetrics_dict:\n {metrics_dict}\n")
-        logging.info(f"\ncheck limits for regularisation:\ndelta_mape 0 ?= {metrics_dict['mape']['ISE_g_regular'][0]} - {best_metrics_dict['mape']['ISE_g_estim'][0]}\n")
-    
-        extr_plots(conf, params, metrics_dict, hyperparams_dict)
-    #else use only first hyperparam from each method config and count errors with them
-    else:
-        y_err = np.array(err_dict[params["y"][0]])
-        for x_temp in params["x"] + params["x_hyp"] + params["y"]:
-            x_err = np.array(err_dict[x_temp])
-            best_metrics_dict["mape"][x_temp] += [mape(x_err, y_err)]
-            best_metrics_dict["rmse"][x_temp] += [rmse(x_err, y_err)]
 
-    logging.info(f"\nbest_metrics_dict = \n{best_metrics_dict}\n")
+    # if needed find best hyperparams and get best_mape/rmse dictionaries with them
+    best_metrics_dict, metrics_dict = count_metrics(params, hyperparams_dict, sizes, err_hyp_dict, err_dict, hyperparams_params["grid_flag"], log)
+    if hyperparams_params["grid_flag"]:
+        extr_plots(conf, params, metrics_dict, hyperparams_dict)
+    log.info(f"\nbest_metrics_dict for max_cov {conf['max_cov']}= \n{best_metrics_dict}\n")
+    
     return best_metrics_dict
 
-def find_sizes(params, hyperparams_dict):
-    sizes = dict()
-    max_size = 0
-    for x_temp in params["x_hyp"]:
-        sizes[x_temp] = len(hyperparams_dict[x_temp])
-        if len(hyperparams_dict[x_temp]) > max_size:
-            max_size = len(hyperparams_dict[x_temp])
-    
-    sizes["max_size"] = max_size
-    return sizes
-
-def errors_init(params, sizes, type):
-    err_dict = {}
-    for x_temp in params["y"] + params["x"]:
-        err_dict[x_temp] = copy(type)
-
-    err_hyp_dict = {}
-    for x_temp in params["x_hyp"]:
-        err_hyp_dict[x_temp] = []
-        for i in range(sizes[x_temp]):
-            err_hyp_dict[x_temp].append(copy(type))
-
-    return err_dict, err_hyp_dict
-
-
-def errors_test(err_dict, err_hyp_dict, conf, params, hyperparams_dict, hyperparams_params, sizes, kf, i):
-    
-    gen_dict = {}
-    gen_dict["f"] = params["f_gen"]()
-    g_sample, gen_dict["g"] = params["g_gen"]()
-    p_sample, gen_dict["p"] = params["p_gen"]()
-    
-    error, error_hyp = errors_init(params, sizes, 0)
-
-    for train_idx, test_idx in kf.split(g_sample):
-
-        logging.info(f"\ntrain_idx : {train_idx},\n test_tdx: {test_idx}\n")
-        gen_dict["g_train"] = g_sample[train_idx]
-        gen_dict["g_test"] = g_sample[test_idx]
-        gen_dict["p_test"] = p_sample[test_idx]
-        
-        params["model_gen"].fit(gen_dict["g_train"], gen_dict["f"](gen_dict["g_train"]))
-
-        gen_dict["err"] = lambda X: np.abs(gen_dict["f"](X) - params["model_gen"].predict(X))
-
-        kde_list = [
-            "ISE_g_estim",
-            "ISE_g_regular",
-            "ISE_g_estim_clip",
-        ]
-        if [i for i in params["x"] + params["x_hyp"] + params["y"] if i in kde_list]:
-            kde_sk = KernelDensity(
-                kernel="gaussian", bandwidth=hyperparams_dict["kde_size"][0]
-            ).fit(gen_dict["g_train"])
-            gen_dict["g_estim"] = lambda X: np.exp(kde_sk.score_samples(X))
-
-        logging.info(f"\ng_estim : {gen_dict['g_estim']}\n")    
-        # work without hyperparams:
-        error_temp = test(
-                    conf,
-                    hyperparams_params,
-                    gen_dict,
-                    params["x"] + params["y"],
-                )
-         
-        for x_temp in params["x"] + params["y"]:
-            error[x_temp] = error[x_temp] + error_temp[x_temp]
-        logging.info(f"\ncurrent error without hyperparams:\n {error}\n")
-        
-        if hyperparams_params["grid_flag"]:
-            for j in trange(sizes['max_size']):
-                hyperparams = {"kde_size": hyperparams_dict["kde_size"][0]}
-                x_estim_temp = []
-                for x_temp in params["x_hyp"]:
-                    if j < sizes[x_temp]:
-                        hyperparams[x_temp] = hyperparams_dict[x_temp][j]
-                        x_estim_temp += [x_temp]
-
-                error_hyp_temp = test(
-                            conf,
-                            hyperparams_params,
-                            gen_dict,
-                            x_estim_temp,
-                            hyperparams,
-                            )
-                
-                for x_temp in x_estim_temp:
-                    error_hyp[x_temp][j] = error_hyp[x_temp][j] + error_hyp_temp[x_temp]
-                logging.info(f"\ncurrent error with hyperparams:\n {error_hyp}\n")    
-        else:
-            logging.info("\ngrid flag is off, no hyp-search:\n")
-            error_hyp_temp = test(
-                            conf,
-                            hyperparams_params,
-                            gen_dict,
-                            params["x_hyp"],
-                            best_hyperparams = {
-                                "kde_size": hyperparams_dict["kde_size"][0],
-                                "n_slices": hyperparams_dict["Mandoline"][0],
-                                "ISE_g_regular": hyperparams_dict["ISE_g_regular"][0],
-                                "ISE_g_clip": hyperparams_dict["ISE_g_clip"][0],
-                                "ISE_g_estim_clip": hyperparams_dict["ISE_g_clip"][0],})
-            
-            for x_temp in params["x_hyp"]:
-                error[x_temp] = error[x_temp] + error_hyp_temp[x_temp]
-            
-            logging.info(f"\ncurrent error for fixed hyperparams:\n {error}\n")
-
-    logging.info(f"\nfinal error for one test num_{i}:\n {error}\n")
-    logging.info(f"\nfinal hyperparams error for one test num_{i}:\n {error_hyp}\n")
-
-    for x_temp in params["x"] + params["y"]:
-        error_temp = error[x_temp]/params["n_splits"]
-        logging.info(f"\nerror_temp in default for {x_temp} = {error_temp}\n")
-        err_dict[x_temp] += [error_temp]
-        logging.info(f"\ntemp error_dict:\n {err_dict}\n")
-        
-    if hyperparams_params["grid_flag"]:
-        for x_temp in params["x_hyp"]:
-            for j in range(sizes['max_size']):
-                if j < sizes[x_temp]:
-                    error_temp = error_hyp[x_temp][j]/params["n_splits"]
-                    logging.info(f"\nerror_temp hyperparams for {x_temp} = {error_temp}\n")
-                    err_hyp_dict[x_temp][j] += [error_temp]  
-            logging.info(f"\nerror_dict_hyp :\n {err_hyp_dict}\n")
-                    
-    else:
-        logging.info("\n for grid_flag = False:\n")
-        for x_temp in params["x_hyp"]:
-            error_temp = error[x_temp]/params["n_splits"]
-            logging.info(f"\nerror_temp hyp:\n {error_temp}\n")
-            err_dict[x_temp] += [error[x_temp]/params["n_splits"]]  
-
-    logging.info(f"\ntest_{i}, current err_dict:\n{err_dict}\n")
-    logging.info(f"\ntest_{i}, current err_hyp_dict:\n{err_hyp_dict}\n")
-    return True
-
-def find_best_params(params, hyperparams_dict, sizes, err_hyp_dict, err_dict):
-    best_hyperparams = {}
-    best_metrics_dict = {"mape": {}, "rmse": {}}
-    metrics_dict = {"mape": {}, "rmse": {}}
-
-    for x_temp in params["x_hyp"]:
-        metrics_dict["mape"][x_temp] = []
-        metrics_dict["rmse"][x_temp] = []
-    
-    for x_temp in params["x"]:
-        best_metrics_dict["mape"][x_temp] = []
-        best_metrics_dict["rmse"][x_temp] = []
-
-    y_err = np.array(err_dict[params["y"][0]])
-    for x_temp in params["x"]:
-        x_err = np.array(err_dict[x_temp])
-        best_metrics_dict["mape"][x_temp] += [mape(x_err, y_err)]
-        best_metrics_dict["rmse"][x_temp] += [rmse(x_err, y_err)]
-    
-    for x_temp in params["x_hyp"]:
-        for i in range(sizes[x_temp]):
-            x_err = np.array(err_hyp_dict[x_temp][i])
-            metrics_dict["mape"][x_temp] += [mape(x_err, y_err)]
-            metrics_dict["rmse"][x_temp] += [rmse(x_err, y_err)]
-
-        best_hyperparams[x_temp] = hyperparams_dict[x_temp][
-            metrics_dict["mape"][x_temp].index(
-                min(metrics_dict["mape"][x_temp])
-            )
-        ]
-
-        best_metrics_dict["mape"][x_temp] = [metrics_dict["mape"][x_temp][
-            metrics_dict["mape"][x_temp].index(
-                min(metrics_dict["mape"][x_temp])
-            )
-        ]]
-        best_metrics_dict["rmse"][x_temp] = [metrics_dict["rmse"][x_temp][
-            metrics_dict["mape"][x_temp].index(
-                min(metrics_dict["mape"][x_temp])
-            )
-        ]]
-
-    logging.info(f"\nbest hyperparams:\n {best_hyperparams}\n")
-    return best_metrics_dict, metrics_dict
-
-
-def extr_plots(conf, params, metrics_hyp_dict, hyperparams_dict):
-    for x_temp in params["x_hyp"]:
-        fig, ax = plt.subplots(figsize=(12, 12))
-        ax.plot(
-            hyperparams_dict[x_temp],
-            metrics_hyp_dict["mape"][x_temp],
-            label=f"{x_temp}",
-        )
-        plt.legend(fontsize=26)
-        plt.title(f"gens_{params['model']}_{params['f']}_max_cov{conf['max_cov']}")
-        ax.set_xlabel(f"param for {x_temp}", fontsize=26)
-        ax.set_ylabel("mape", fontsize=26)
-        #ax.set_xscale('log')
-        plt.savefig(
-            f"./plots/results/{params['model']}_{params['f']}_{x_temp}_{conf['max_cov']}.pdf"
-        )
-        plt.tight_layout()
-        # plt.show()

source/simulation.py
@@ -1,8 +1,8 @@
-import numpy as np
+import GPy
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.linalg import cholesky, solve_triangular
 from sklearn.mixture import GaussianMixture
-import GPy
 
 np.seterr(divide="ignore")
 np.random.seed(42)
@@ -74,9 +74,9 @@ def random_GMM_samples(config):
     )
     samples = gmm.sample(config["n_samples"])
 
-    density = lambda X: np.exp(gmm.score_samples(X))
+    log_density = lambda X: gmm.score_samples(X)
 
-    return samples[0], density
+    return samples[0], log_density
 
 
 def random_uniform_samples(config, fixed_region: bool = False):
@@ -98,11 +98,10 @@ def random_uniform_samples(config, fixed_region: bool = False):
     w_a = np.random.random((config["n_samples"], config["n_dim"]))
     w_b = 1 - w_a
 
-    density = (
-        lambda X: np.exp(np.log(((a < X) & (X < b)).all(axis=1)) - np.log((b - a)).sum())
-    )
+    log_density = lambda X: np.log(((a < X) & (X < b)).all(axis=1)) - np.log((b - a)).sum()
+    
 
-    return w_a * a + w_b * b, density
+    return w_a * a + w_b * b, log_density
 
 
 def random_GP_func(config):
@@ -117,7 +116,7 @@ def random_GP_func(config):
 
     fun = lambda X: model.posterior_samples_f(X, full_cov=True, size=1)
 
-    # heatmap(config, fun, n_points = 50)
+    #heatmap(config, fun, n_points = 50)
 
     return fun
 
@@ -137,8 +136,8 @@ def heatmap(config, fun, n_points=50):
     plt.colorbar(label="Prediction")
     plt.title("Gaussian Process Prediction")
     plt.savefig("./plots/results/GP.pdf")
-    plt.xlabel("X1")
-    plt.ylabel("X2")
+    plt.xlabel("X")
+    plt.ylabel("Y")
     # plt.show()
 
 

tests/test.py
@@ -1,17 +1,6 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import logging
-
-# from scipy.stats import gaussian_kde
-
-from source.estimations import (
-    importance_sampling_error,
-    monte_carlo_error,
-    ISE_clip,
-)
-
+from source.estimations import (ISE_clip, importance_sampling_error,
+                                monte_carlo_error)
 from source.mandoline_estimation import mandoline_error
-
 
 def test(
     conf,
@@ -38,55 +27,54 @@ def test(
 
     if "ISE_g" in target_error:
 
-        iter_err["ISE_g"] = importance_sampling_error(gen_dict["err"], gen_dict["p"], gen_dict["g"], gen_dict["g_test"])
+        iter_err["ISE_g"] = importance_sampling_error(
+            gen_dict["err"], gen_dict["p"], gen_dict["g"], gen_dict["g_test"]
+        )
 
     if "ISE_g_estim" in target_error:
         iter_err["ISE_g_estim"] = importance_sampling_error(
-                gen_dict["err"],
-                gen_dict["p"],
-                gen_dict["g_estim"],
-                gen_dict["g_test"],
-            )
-        
-        
+            gen_dict["err"],
+            gen_dict["p"],
+            gen_dict["g_estim"],
+            gen_dict["g_test"],
+        )
+
     if "ISE_g_regular" in target_error:
         epsilon = hyperparams["ISE_g_regular"]
         g_estim_new = lambda X: (1 - epsilon) * gen_dict["g_estim"](X) + epsilon / (
             conf["max_mu"] ** 2
         )
-        
+
         iter_err["ISE_g_regular"] = importance_sampling_error(
-                gen_dict["err"],
-                gen_dict["p"],
-                g_estim_new,
-                gen_dict["g_test"],
-            )
-         
+            gen_dict["err"],
+            gen_dict["p"],
+            g_estim_new,
+            gen_dict["g_test"],
+        )
+
     if "ISE_g_clip" in target_error:
         iter_err["ISE_g_clip"] = ISE_clip(
-                gen_dict["err"],
-                gen_dict["p"],
-                gen_dict["g"],
-                gen_dict["g_test"],
-                hyperparams["ISE_g_clip"],
-                smooth_flag=hyperparams_params["smooth_flag"],
-            )
-        
+            gen_dict["err"],
+            gen_dict["p"],
+            gen_dict["g"],
+            gen_dict["g_test"],
+            hyperparams["ISE_g_clip"],
+            smooth_flag=hyperparams_params["smooth_flag"],
+        )
 
     if "ISE_g_estim_clip" in target_error:
         iter_err["ISE_g_estim_clip"] = ISE_clip(
-                gen_dict["err"],
-                gen_dict["p"],
-                gen_dict["g_estim"],
-                gen_dict["g_test"],
-                hyperparams["ISE_g_estim_clip"],
-                smooth_flag=hyperparams_params["smooth_flag"],
-            )
-        
+            gen_dict["err"],
+            gen_dict["p"],
+            gen_dict["g_estim"],
+            gen_dict["g_test"],
+            hyperparams["ISE_g_estim_clip"],
+            smooth_flag=hyperparams_params["smooth_flag"],
+        )
 
     if "Mandoline" in target_error:
         iter_err["Mandoline"] = mandoline_error(
-                gen_dict, n_slices=hyperparams["Mandoline"]
-            )
-        
+            gen_dict, n_slices=hyperparams["Mandoline"], slice_method=hyperparams_params['slice_method']
+        )
+
     return iter_err