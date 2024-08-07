fixed problems with KL, started to redact bw-search for similar generations

source/LSCV.py
@@ -2,7 +2,7 @@ import numpy as np
 import statsmodels.api as sm  # for library lscv, beta = 0
 import logging
 from scipy.integrate import dblquad
-from scipy.optimize import minimize
+from scipy.optimize import minimize, basinhopping
 from sklearn.neighbors import KernelDensity
 
 log = logging.getLogger("__main__")
@@ -15,26 +15,28 @@ def u_mean(log_f_n, p_sample):
 def squared_error_sklearn(h, conf, g_sample, p_sample, beta):
     h = h[0]
     kde = KernelDensity(kernel="gaussian", bandwidth=h).fit(g_sample)
-    f_n_squared = lambda x, y: (np.exp(kde.score_samples(np.array([[x, y]])))) ** 2
+    #f_n_squared = lambda x, y: (np.exp(kde.score_samples(np.array([[x, y]])))) ** 2
     log_f_n = lambda X: (kde.score_samples(X))
 
-    def f_sub_sample_mean():
-        summation = 0
-        for i in range(len(g_sample)):
-            subsample = np.delete(g_sample, i, axis=0)
+    #def f_sub_sample_mean():
+    #    summation = 0
+    #    for i in range(len(g_sample)):
+    #        subsample = np.delete(g_sample, i, axis=0)
 
-            kde = KernelDensity(kernel="gaussian", bandwidth=h).fit(subsample)
-            predict_value = np.exp(kde.score_samples(g_sample[i].reshape(1, -1)))
-            summation += predict_value
-        return summation / len(g_sample)
+    #        kde = KernelDensity(kernel="gaussian", bandwidth=h).fit(subsample)
+    #        predict_value = np.exp(kde.score_samples(g_sample[i].reshape(1, -1)))
+    #        summation += predict_value
+    #    return summation / len(g_sample)
 
-    integral = dblquad(f_n_squared, 0, conf["max_mu"], 0, conf["max_mu"])
-    uniform_sum = u_mean(log_f_n, p_sample)
-    sub = f_sub_sample_mean()
-    cv = integral[0] - 2 * sub - beta * uniform_sum
-    log.debug(
-        f"integral = {integral}, subsample = {sub}, uniform_sum = {uniform_sum}, cv = {cv}"
-    )
+    #integral = dblquad(f_n_squared, 0, conf["max_mu"], 0, conf["max_mu"])
+    uniform_sum = beta*u_mean(log_f_n, p_sample)
+    #sub = f_sub_sample_mean()
+    #cv = integral[0] - 2 * sub - uniform_sum
+    cv = -uniform_sum
+    #log.debug(
+    #    f"integral = {integral}, subsample = {sub}, beta*uniform_sum = {uniform_sum}, cv = {cv}"
+    #)
+    log.debug(f"cv = {cv}")
     return cv
 
 
@@ -43,12 +45,7 @@ def KL_LSCV_find_bw(conf, g_sample, p_sample, beta=0):
 
     log.debug(f"Initial h0: {h0}")
     cons = {"type": "ineq", "fun": lambda x: x[0] - 10 ** (-8)}
-
-    h = minimize(
-        squared_error_sklearn,
-        h0,
-        args=(conf, g_sample, p_sample, beta),
-        constraints=cons,
-    ).x
+    
+    h = minimize(squared_error_sklearn, h0, args=(conf, g_sample, p_sample, beta), constraints=cons).x
 
     return h[0]

source/errors.py
@@ -31,20 +31,23 @@ def errors_test(
     p_sample, gen_dict["p"] = params["p_gen"]()
     gen_dict["model"] = params["model_gen"]
 
-    error, error_hyp = errors_init(params, sizes, 0, hyperparams_params["grid_flag"])
+    error, error_hyp = errors_init(params, sizes, 0)
 
     for train_idx, test_idx in kf.split(g_sample):
         log.debug(f"\ntrain_idx : {train_idx},\n test_tdx: {test_idx}\n")
 
         fill_gen_dict(
+            conf,
             gen_dict,
             params,
             hyperparams_dict,
+            hyperparams_params,
             g_sample,
             p_sample,
             train_idx,
             test_idx,
         )
+
         fill_errors(
             error,
             error_hyp,
@@ -69,7 +72,7 @@ def errors_test(
 
 
 def fill_gen_dict(
-    gen_dict, params, hyperparams_dict, g_sample, p_sample, train_idx, test_idx
+    conf,gen_dict, params, hyperparams_dict, hyperparams_params, g_sample, p_sample, train_idx, test_idx
 ):
     log = logging.getLogger("__main__")
     gen_dict["g_train"] = g_sample[train_idx]
@@ -82,25 +85,18 @@ def fill_gen_dict(
         np.abs(gen_dict["f"](X) - params["model_gen"].predict(X))
     )
 
-    kde_list = [
-        "ISE_g_estim",
-        "ISE_g_estim_KL",
-        "ISE_g_reg_uniform",
-        "ISE_g_reg_degree",
-        "ISE_g_estim_clip",
-    ]
+    if([s for s in params['x'] + params['x_hyp'] if "estim" in s or "reg" in s]):
+        bw = hyperparams_dict["bandwidth"]
+        if hyperparams_dict["bandwidth"] == 'KL_LSCV':
+            bw = KL_LSCV_find_bw(conf, gen_dict["g_train"], gen_dict["p_test"], hyperparams_params["beta"])
 
-    bw = hyperparams_dict["bandwidth"]
-    if [
-        i for i in params["x"] + params["x_hyp"] + params["y"] if i in kde_list
-    ] and bw != "KL_LSCV":
         kde_sk = KernelDensity(kernel="gaussian", bandwidth=bw).fit(gen_dict["g_train"])
 
-        if bw == "scott" or bw == "silverman":
+        if hyperparams_dict["bandwidth"] == "scott" or hyperparams_dict["bandwidth"] == "silverman":
             hyperparams_dict["bandwidth"] = kde_sk.bandwidth_
-            log.debug(f"current for scott/silvername bandwidth is: {bw}")
+            log.debug(f"current for scott/silvername bandwidth is: {hyperparams_dict['bandwidth']}")
+        
         gen_dict["g_estim"] = lambda X: kde_sk.score_samples(X)
-
         log.debug(f"\ng_estim : {gen_dict['g_estim']}\n")
     return True
 
@@ -116,7 +112,7 @@ def fill_errors(
     gen_dict,
 ):
     log = logging.getLogger("__main__")
-    # work without hyperparams:
+
     error_temp = test(
         conf,
         hyperparams_params,
@@ -126,96 +122,42 @@ def fill_errors(
 
     for x_temp in params["x"] + params["y"]:
         error[x_temp] = error[x_temp] + np.exp(error_temp[x_temp])
+    
     log.debug(
         f"\ncurrent errors_dict for one test, summing for folds (no hyp methods):\n {error}\n"
     )
 
-    # for hyperparams
-    if hyperparams_params["grid_flag"]:
-        bw_list = []
-        for j in trange(sizes["max_size"]):
-            hyperparams = {"kde_size": hyperparams_dict["bandwidth"]}
-            x_estim_temp = []
-            for x_temp in params["x_hyp"]:
-                if j < sizes[x_temp]:
-                    hyperparams[x_temp] = hyperparams_dict[x_temp][j]
-                    x_estim_temp += [x_temp]
+    for j in trange(sizes["max_size"]):
+        hyperparams = {"kde_size": hyperparams_dict["bandwidth"]}
+        x_estim_temp = []
+        for x_temp in params["x_hyp"]:
+            if j < sizes[x_temp]:
+                hyperparams[x_temp] = hyperparams_dict[x_temp][j]
+                x_estim_temp += [x_temp]
 
-            if hyperparams_dict["bandwidth"] == "KL_LSCV":
-                bw = KL_LSCV_find_bw(
-                    conf,
-                    gen_dict["g_train"],
-                    gen_dict["p_test"],
-                    beta=hyperparams["ISE_g_estim_KL"],
-                )
-                log.debug(
-                    f"\ncurrent bandwidth for KL_LSCV with beta = {hyperparams['ISE_g_estim_KL']} is: {bw}\n"
-                )
-
-                kde_sk = KernelDensity(kernel="gaussian", bandwidth=bw).fit(
-                    gen_dict["g_train"]
-                )
-                gen_dict["g_estim"] = lambda X: kde_sk.score_samples(X)
-
-            error_hyp_temp = test(
-                conf,
-                hyperparams_params,
-                gen_dict,
-                x_estim_temp,
-                hyperparams,
-            )
-            
-
-            if 'ISE_g_clip' in x_temp and j < sizes["ISE_g_clip"] and hyperparams_dict["ISE_g_clip"][j] == "quantile":
-                hyperparams_dict["ISE_g_clip"][j] == hyperparams["ISE_g_clip"]
-            
-            if 'USE_g_estim_clip' in x_temp and j < sizes["ISE_g_estim_clip"] and hyperparams_dict["ISE_g_estim_clip"][j] == "quantile":
-                hyperparams_dict["ISE_g_estim_clip"][j] == hyperparams["ISE_g_estim_clip"]
-
-            for x_temp in x_estim_temp:
-                error_hyp[x_temp][j] = error_hyp[x_temp][j] + np.exp(
-                    error_hyp_temp[x_temp]
-                )
-
-        log.debug(
-            f"\ncurrent error for one test (sum for folds) with hyperparams:\n {error_hyp}\n"
-        )
-
-    else:
-        log.debug(f"\ngrid flag is off, no hyp-search:\n")
-
-        if hyperparams_dict["bandwidth"] == "KL_LSCV":
-            bw = KL_LSCV_find_bw(
-                gen_dict["g_train"], beta=hyperparams["ISE_g_estim_KL"]
-            )
-            log.debug(
-                f"\ncurrent bandwidth for KL_LSCV with beta = {hyperparams['ISE_g_estim_KL']} is: {bw}\n"
-            )
-
-            kde_sk = KernelDensity(kernel="gaussian", bandwidth=bw).fit(
-                gen_dict["g_train"]
-            )
-            gen_dict["g_estim"] = lambda X: kde_sk.score_samples(X)
-
+       
         error_hyp_temp = test(
             conf,
             hyperparams_params,
             gen_dict,
-            params["x_hyp"],
-            hyperparams={
-                "ISE_g_reg_uniform": hyperparams_dict["ISE_g_reg_uniform"][0],
-                "ISE_g_reg_degree": hyperparams_dict["ISE_g_reg_degree"][0],
-                "ISE_g_clip": hyperparams_dict["ISE_g_clip"][0],
-                "ISE_g_estim_clip": hyperparams_dict["ISE_g_clip"][0],
-            },
+            x_estim_temp,
+            hyperparams,
         )
+        
+        for x_temp in x_estim_temp:
+            if x_temp in ['ISE_g_estim_clip, ISE_g_estim_clip_KL', 'ISE_g_clip']:
+                if hyperparams_dict[x_temp][j] == "quantile":
+                    hyperparams_dict[x_temp][j] == hyperparams[x_temp]
+        
 
-        for x_temp in params["x_hyp"]:
-            error[x_temp] = error[x_temp] + np.exp(error_hyp_temp[x_temp])
+        for x_temp in x_estim_temp:
+            error_hyp[x_temp][j] = error_hyp[x_temp][j] + np.exp(
+                error_hyp_temp[x_temp]
+            )
 
-        log.debug(
-            f"\ncurrent error for one test sum for folds for fixed hyperparams:\n {error}\n"
-        )
+    log.debug(
+        f"\ncurrent error for one test (sum for folds) with hyperparams:\n {error_hyp}\n"
+    )
 
     return True
 
@@ -231,29 +173,20 @@ def fill_dicts(
         f"\ntemp error_dict (average errors for folds) for one test:\n {err_dict}\n"
     )
 
-    if hyperparams_params["grid_flag"]:
-        for x_temp in params["x_hyp"]:
-            for j in range(sizes["max_size"]):
-                if j < sizes[x_temp]:
-                    err_hyp_dict[x_temp][j] += [
-                        error_hyp[x_temp][j] / params["n_splits"]
-                    ]
-        log.debug(
-            f"\nerror_dict_hyp (average errors for folds) for one test:\n {err_hyp_dict}\n"
-        )
-
-    else:
-        log.debug(f"\n without grid-search:\n")
-        for x_temp in params["x_hyp"]:
-            err_dict[x_temp] += [error[x_temp] / params["n_splits"]]
-        log.debug(
-            f"\nerror_dict (average errors for folds) for one test:\n {err_dict}\n"
-        )
-
+    for x_temp in params["x_hyp"]:
+        for j in range(sizes["max_size"]):
+            if j < sizes[x_temp]:
+                err_hyp_dict[x_temp][j] += [
+                    error_hyp[x_temp][j] / params["n_splits"]
+                ]
+    log.debug(
+        f"\nerror_dict_hyp (average errors for folds) for one test:\n {err_hyp_dict}\n"
+    )
+    
     return True
 
 
-def count_metrics(params, hyperparams_dict, sizes, err_hyp_dict, err_dict, flag):
+def count_metrics(params, hyperparams_dict, sizes, err_hyp_dict, err_dict):
     log = logging.getLogger("__main__")
 
     best_hyperparams = {}
@@ -270,30 +203,25 @@ def count_metrics(params, hyperparams_dict, sizes, err_hyp_dict, err_dict, flag)
         metrics_dict["mape"][x_temp] = []
         metrics_dict["rmse"][x_temp] = []
 
-    if flag:
-        for x_temp in params["x_hyp"]:
-            for i in range(sizes[x_temp]):
-                x_err = np.array(err_hyp_dict[x_temp][i])
-                metrics_dict["mape"][x_temp] += [mape(x_err, y_err)]
-                metrics_dict["rmse"][x_temp] += [rmse(x_err, y_err)]
 
-            best_hyperparams[x_temp] = hyperparams_dict[x_temp][
-                metrics_dict["mape"][x_temp].index(min(metrics_dict["mape"][x_temp]))
-            ]
+    for x_temp in params["x_hyp"]:
+        for i in range(sizes[x_temp]):
+            x_err = np.array(err_hyp_dict[x_temp][i])
+            metrics_dict["mape"][x_temp] += [mape(x_err, y_err)]
+            metrics_dict["rmse"][x_temp] += [rmse(x_err, y_err)]
 
-            best_metrics_dict["mape"][x_temp] = metrics_dict["mape"][x_temp][
-                metrics_dict["mape"][x_temp].index(min(metrics_dict["mape"][x_temp]))
-            ]
-            best_metrics_dict["rmse"][x_temp] = metrics_dict["rmse"][x_temp][
-                metrics_dict["mape"][x_temp].index(min(metrics_dict["mape"][x_temp]))
-            ]
+        best_hyperparams[x_temp] = hyperparams_dict[x_temp][
+            metrics_dict["mape"][x_temp].index(min(metrics_dict["mape"][x_temp]))
+        ]
 
-        log.info(f"\nbest hyperparams:\n {best_hyperparams}\n")
-        log.debug(f"\nmetrics_dict:\n {metrics_dict}\n")
-    else:
-        for x_temp in params["x_hyp"]:
-            x_err = np.array(err_dict[x_temp])
-            best_metrics_dict["mape"][x_temp] = mape(x_err, y_err)
-            best_metrics_dict["rmse"][x_temp] = rmse(x_err, y_err)
+        best_metrics_dict["mape"][x_temp] = metrics_dict["mape"][x_temp][
+            metrics_dict["mape"][x_temp].index(min(metrics_dict["mape"][x_temp]))
+        ]
+        best_metrics_dict["rmse"][x_temp] = metrics_dict["rmse"][x_temp][
+            metrics_dict["mape"][x_temp].index(min(metrics_dict["mape"][x_temp]))
+        ]
 
+    log.info(f"\nbest hyperparams:\n {best_hyperparams}\n")
+    log.debug(f"\nmetrics_dict:\n {metrics_dict}\n")
+    
     return best_metrics_dict, metrics_dict

source/estimations.py
@@ -28,18 +28,18 @@ def monte_carlo_error(err, p_sample):
 
 def clip(a, b_min, b_max):
     if a < b_min:
-        return [np.log(b_min), 1]
+        return b_min, 1
     elif a > b_max:
-        return [np.log(b_max), 1]
+        return b_max, 1
     else:
-        return [np.log(a), 0]
+        return a, 0
 
 
 def smooth_clip(x, eps):
     return (1 + eps) / (1 + (2 * eps / (1 - eps)) * np.exp(-x))
 
 
-def ISE_clip(err, p, g, g_sample, eps, smooth_flag=True, delta = 0.1, thrhold = 0.95):
+def ISE_clip(err, p, g, g_sample, eps, smooth_flag=True, thrhold = 0.95, clip_step = 0.001):
     """
     :param err: log-error function
     :param p: log-probability density of target distribution
@@ -48,31 +48,33 @@ def ISE_clip(err, p, g, g_sample, eps, smooth_flag=True, delta = 0.1, thrhold = 
     :param eps: epsilon for clip
     :return: log-ISE with clip
     """
-    clipped_array = []
-
     log = logging.getLogger("__main__")
     if(eps == "quantile"):
-        num_minmax = 0
-        for eps_temp in np.arange(0, 1, delta):
-            clipped_array = []       
+        for eps_temp in np.arange(1 - clip_step, -clip_step, -clip_step):
+            clipped_array = []
+            num_clipped = 0       
             for p_elem, g_elem in zip(p(g_sample), g(g_sample)):
                 weight = np.exp(p_elem - g_elem)
-                elem = clip(weight, eps_temp)
-                num_minmax = num_minmax + elem[1]
-                clipped_array.append(elem[0])
-           
-            if (num_minmax/g_sample.shape[0] <= thrhold):
+                clipped_weight, i = clip(weight, 1-eps_temp, 1+eps_temp)
+                clipped_array.append(np.log(clipped_weight))
+                num_clipped = num_clipped + i
+
+            #log.debug(f"for eps = {eps_temp} got num = {num_clipped} instead of {len(g_sample) * thrhold}")
+            if num_clipped > len(g_sample) * thrhold:
                 eps = eps_temp
-            else:
-                log.info(f"thrhold didnt reached, used MCE_g max value eps!")
+                log.debug(f"eps_clip_quantile = {eps}")
+                break
+    else:
+        clipped_array = []
+        for p_elem, g_elem in zip(p(g_sample), g(g_sample)):
+                weight = np.exp(p_elem - g_elem)
+                clipped_weight, i = clip(weight, 1-eps_temp, 1+eps_temp)
+                clipped_array.append(np.log(clipped_weight))
     
     if smooth_flag:
         clipped_array = []
         for p_elem, g_elem in zip(p(g_sample), g(g_sample)):
             weight = np.exp(p_elem - g_elem)
-            if np.isnan(weight):
-                logging.info(f"weight is: weight")
-            else:
-                clipped_array.append(np.log(smooth_clip(weight, eps)))
+            clipped_array.append(np.log(smooth_clip(weight, eps)))
 
     return logsumexp(clipped_array + err(g_sample)) - np.log(g_sample.shape[0])